adc654df138ff2ebb93edd48248b92ca761bdf4b
GoZillaTiAmO <gozilla.tiamo@gmail.com>
2016-11-03 18:52:56 +0700

    Commit New Screen

--- app/src/main/java/com/worldwidewealth/wealthcounter/FragmentRegister.java
+++ app/src/main/java/com/worldwidewealth/wealthcounter/FragmentRegister.java
@@ -7,18 +7,20 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by gozillatiamo on 10/3/16.
  */
-public class FragmentRegister1 extends Fragment {
+public class FragmentRegister extends Fragment {
 
     private View rootView;
     private ViewHolder mHolder;
 
     public static Fragment newInstance(){
-        FragmentRegister1 fragment = new FragmentRegister1();
+        FragmentRegister fragment = new FragmentRegister();
         return fragment;
     }
 
@@ -27,7 +29,7 @@ public class FragmentRegister1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         if (rootView == null){
-            rootView = inflater.inflate(R.layout.fragment_register1, null, false);
+            rootView = inflater.inflate(R.layout.fragment_register, null, false);
             mHolder = new ViewHolder(rootView);
             rootView.setTag(mHolder);
         } else mHolder = (ViewHolder) rootView.getTag();
@@ -35,16 +37,23 @@ public class FragmentRegister1 extends Fragment {
         mHolder.mBtnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = FragmentRegister1.this.getActivity()
+                FragmentTransaction transaction = FragmentRegister2.this.getActivity()
                         .getSupportFragmentManager()
                         .beginTransaction()
-                        .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_left, R.anim.slide_out_right)
-                        .replace(R.id.main_container, FragmentRegister2.newInstance())
+                        .setCustomAnimations(R.anim.dialog_enter, R.anim.dialog_exit, R.anim.slide_in_left, R.anim.slide_out_right)
+                        .replace(R.id.main_container, FragmentRegisterDone.newInstance())
                         .addToBackStack(null);
 
                 transaction.commit();
             }
         });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.type_people_dropdown, R.layout.text_spinner);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mHolder.mSpinnerTypePeople.setAdapter(adapter);
+        mHolder.mSpinnerTypePeople.setSelection(0);
+
 
         return rootView;
     }
@@ -52,9 +61,11 @@ public class FragmentRegister1 extends Fragment {
     public class ViewHolder{
 
         private Button mBtnNext;
+        private Spinner mSpinnerTypePeople;
         public ViewHolder(View view){
 
             mBtnNext = (Button) view.findViewById(R.id.btn_next);
+            mSpinnerTypePeople = (Spinner) view.findViewById(R.id.spinner_type_people);
         }
     }
 
---
+++ app/src/main/java/com/worldwidewealth/wealthcounter/game/ActivityGame.java
@@ -0,0 +1,13 @@
+package com.worldwidewealth.wealthcounter.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
