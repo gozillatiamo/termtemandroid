8690dc03cd9fe8447b498e7723090bf4ce3855dd
GoZillaTiAmO <gozilla.tiamo@gmail.com>
2016-11-18 19:25:18 +0700

    commit new encryption

---
+++ app/src/main/java/com/worldwidewealth/wealthcounter/MyApplication.java
@@ -0,0 +1,8 @@
+package com.worldwidewealth.wealthcounter;
+
+/**
+ * Created by MyNet on 17/11/2559.
+ */
+
+public class MyApplication {
+}
---
+++ app/src/main/java/com/worldwidewealth/wealthcounter/until/ErrorNetworkThrowable.java
@@ -0,0 +1,8 @@
+package com.worldwidewealth.wealthcounter.until;
+
+/**
+ * Created by MyNet on 17/11/2559.
+ */
+
+public class ErrorNetworkThrowable {
+}
